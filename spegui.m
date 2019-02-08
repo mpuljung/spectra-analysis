a8fe6af8f98a9665b54434c06950ac5c721b2830
mpuljung <47451444+mpuljung@users.noreply.github.com>
2019-02-08 13:26:08 +0000

    Update spegui.m

--- spegui.m
+++ spegui.m
@@ -1,3 +1,17 @@
+%Copyright 2019 Michael Puljung, Samuel Usher
+
+   %Licensed under the Apache License, Version 2.0 (the "License");
+   %you may not use this file except in compliance with the License.
+   %You may obtain a copy of the License at
+
+    % http://www.apache.org/licenses/LICENSE-2.0
+
+   %Unless required by applicable law or agreed to in writing, software
+   %distributed under the License is distributed on an "AS IS" BASIS,
+   %WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   %See the License for the specific language governing permissions and
+   %limitations under the License.
+
 function spegui
 %this command starts a gui to select *.spe files for analysis
 %designate files for bleaching correction, as desired
@@ -6,6 +20,9 @@ function spegui
 %data are corrected
 %final output is a *.xlsx file with the raw data, subtracted data, and
 %bleaching corrected subtracted data.
+
+
+
 close all force
 [filename,pathname]=uigetfile('.spe','Select Files to Analyze','C:\Users\dpag0575\Documents\Data\Imaging\Pixis','MultiSelect','on');
 cd(pathname);
